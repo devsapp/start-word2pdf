558174fddafc10db3c29329ab7a5dadb85c0c196
rsonghuster <ls_huster@163.com>
2023-11-17 19:45:26 +0800

    change to fc3.0

--- src/code/app.py
+++ src/code/app.py
@@ -1,5 +1,5 @@
 from reportlab.pdfbase import pdfmetrics, ttfonts
-from PyPDF2 import PdfReader , PdfWriter 
+from PyPDF2 import PdfReader, PdfWriter
 from reportlab.lib.units import cm
 from reportlab.pdfgen import canvas
 import logging
@@ -10,25 +10,32 @@ import oss2
 import os
 import json
 from flask import Flask, request
+
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
 # support chinese
-pdfmetrics.registerFont(ttfonts.TTFont('zenhei', os.path.join(
-    "/usr/share/fonts/truetype/wqy", 'wqy-zenhei.ttc')))
-pdfmetrics.registerFont(ttfonts.TTFont('microhei', os.path.join(
-    "/usr/share/fonts/truetype/wqy", 'wqy-microhei.ttc')))
+pdfmetrics.registerFont(
+    ttfonts.TTFont(
+        "zenhei", os.path.join("/usr/share/fonts/truetype/wqy", "wqy-zenhei.ttc")
+    )
+)
+pdfmetrics.registerFont(
+    ttfonts.TTFont(
+        "microhei", os.path.join("/usr/share/fonts/truetype/wqy", "wqy-microhei.ttc")
+    )
+)
 
 
 def add_watermark(pdf_file_in, pdf_file_mark, pdf_file_out):
     print(pdf_file_in, pdf_file_mark, pdf_file_out)
     pdf_output = PdfWriter()
-    with open(pdf_file_in, 'rb') as input_stream:
+    with open(pdf_file_in, "rb") as input_stream:
         pdf_input = PdfReader(input_stream, strict=False)
 
         pageNum = len(pdf_input.pages)
-        pdf_watermark = PdfReader(open(pdf_file_mark, 'rb'), strict=False)
+        pdf_watermark = PdfReader(open(pdf_file_mark, "rb"), strict=False)
 
         for i in range(pageNum):
             page = pdf_input.pages[i]
@@ -36,11 +43,20 @@ def add_watermark(pdf_file_in, pdf_file_mark, pdf_file_out):
             page.compress_content_streams()
             pdf_output.add_page(page)
 
-        with open(pdf_file_out, 'wb') as f:
+        with open(pdf_file_out, "wb") as f:
             pdf_output.write(f)
 
 
-def _create_watermark(mark_text, pagesize=(21*cm, 29.7*cm), font="Helvetica", font_size=30, font_color=(0, 0, 0), rotate=0, opacity=1, density=(5*cm, 5*cm)):
+def _create_watermark(
+    mark_text,
+    pagesize=(21 * cm, 29.7 * cm),
+    font="Helvetica",
+    font_size=30,
+    font_color=(0, 0, 0),
+    rotate=0,
+    opacity=1,
+    density=(5 * cm, 5 * cm),
+):
     file_name = "/tmp/mark.pdf"
     c = canvas.Canvas(file_name, pagesize=pagesize)
     c.setFont(font, font_size)
@@ -51,38 +67,38 @@ def _create_watermark(mark_text, pagesize=(21*cm, 29.7*cm), font="Helvetica", fo
     c.setFillAlpha(opacity)
 
     row_gap, col_gap = density
-    colN = int(pagesize[0]/col_gap)
-    rowN = int(pagesize[1]/row_gap)
+    colN = int(pagesize[0] / col_gap)
+    rowN = int(pagesize[1] / row_gap)
     x = colN * 4
     y = rowN * 4
 
     for i in range(y):
         for j in range(x):
-            a = col_gap*(j - 2*colN)
-            b = row_gap*(i - 2*rowN)
+            a = col_gap * (j - 2 * colN)
+            b = row_gap * (i - 2 * rowN)
             c.drawString(a, b, mark_text)
 
     c.save()
 
 
 def create_watermark(evt):
-    mark_text = evt['mark_text']
+    mark_text = evt["mark_text"]
     # 1cm = 28.346456692913385， defalut is A4, (21*cm, 29.7*cm)
-    pagesize = evt.get(
-        'pagesize', [595.275590551181, 841.8897637795275])
-    font = evt.get('font', 'Helvetica')
-    font_size = evt.get('font_size', 30)
-    font_color = evt.get('font_color', (0, 0, 0))
-    rotate = evt.get('rotate', 0)
-    opacity = evt.get('opacity', 0.1)
+    pagesize = evt.get("pagesize", [595.275590551181, 841.8897637795275])
+    font = evt.get("font", "Helvetica")
+    font_size = evt.get("font_size", 30)
+    font_color = evt.get("font_color", (0, 0, 0))
+    rotate = evt.get("rotate", 0)
+    opacity = evt.get("opacity", 0.1)
     # default is (7*cm, 10*cm)
-    density = evt.get('density', [198.4251968503937, 283.46456692913387])
-    _create_watermark(mark_text, pagesize, font, font_size,
-                      font_color, rotate, opacity, density)
-    print('create_watermark success!')
+    density = evt.get("density", [198.4251968503937, 283.46456692913387])
+    _create_watermark(
+        mark_text, pagesize, font, font_size, font_color, rotate, opacity, density
+    )
+    print("create_watermark success!")
 
 
-@app.route('/invoke', methods=['POST'])
+@app.route("/invoke", methods=["POST"])
 def invoke():
     # See FC docs for all the HTTP headers: https://www.alibabacloud.com/help/doc-detail/132044.htm#common-headers
     request_id = request.headers.get("x-fc-request-id", "")
@@ -97,7 +113,7 @@ def invoke():
     # access_key_secret = request.headers['x-fc-access-key-secret']
     # access_security_token = request.headers['x-fc-security-token']
 
-    region = request.headers['x-fc-region']
+    region = request.headers["x-fc-region"]
     oss_endpoint = "http://oss-{}-internal.aliyuncs.com".format(region)
 
     # do your things
@@ -107,15 +123,23 @@ def invoke():
         word_file = evt["word_file"]
         fileDir, tempfilename = os.path.split(word_file)
         shortname, _ = os.path.splitext(tempfilename)
-        accessKeyId = request.headers['x-fc-access-key-id']
-        accessKeySecret = request.headers['x-fc-access-key-secret']
-        securityToken = request.headers['x-fc-security-token']
+        accessKeyId = request.headers["x-fc-access-key-id"]
+        accessKeySecret = request.headers["x-fc-access-key-secret"]
+        securityToken = request.headers["x-fc-security-token"]
         auth = oss2.StsAuth(accessKeyId, accessKeySecret, securityToken)
-        bucket = oss2.Bucket(auth, oss_endpoint, os.environ['OSS_BUCKET'])
-        bucket.get_object_to_file(word_file, '/tmp/' + tempfilename)
-        subprocess.check_call(["soffice", "--convert-to", "pdf:writer_pdf_Export", "--outdir",
-                               "/tmp", '/tmp/' + tempfilename])
-        print('word to pdf  success!')
+        bucket = oss2.Bucket(auth, oss_endpoint, os.environ["OSS_BUCKET"])
+        bucket.get_object_to_file(word_file, "/tmp/" + tempfilename)
+        subprocess.check_call(
+            [
+                "soffice",
+                "--convert-to",
+                "pdf:writer_pdf_Export",
+                "--outdir",
+                "/tmp",
+                "/tmp/" + tempfilename,
+            ]
+        )
+        print("word to pdf  success!")
         pdf_file = os.path.join(fileDir, shortname + ".pdf")
         local_pdf_file = "/tmp/" + shortname + ".pdf"
 
@@ -123,13 +147,11 @@ def invoke():
         has_watermark = evt.get("mark_text")
         if has_watermark:
             create_watermark(evt)
-            local_pdf_file = '/tmp/' + shortname + "_out.pdf"
-            add_watermark('/tmp/' + shortname + ".pdf",
-                          '/tmp/mark.pdf', local_pdf_file)
-            print('pdf add_watermark success!')
+            local_pdf_file = "/tmp/" + shortname + "_out.pdf"
+            add_watermark("/tmp/" + shortname + ".pdf", "/tmp/mark.pdf", local_pdf_file)
+            print("pdf add_watermark success!")
 
-        result = bucket.put_object_from_file(
-            pdf_file, local_pdf_file)
+        result = bucket.put_object_from_file(pdf_file, local_pdf_file)
 
         subprocess.check_call(["ls", "-ll", "/tmp"])
         subprocess.check_call("rm -rf /tmp/*", shell=True)
@@ -144,15 +166,15 @@ def invoke():
     except Exception as e:
         exc_info = sys.exc_info()
         trace = traceback.format_tb(exc_info[2])
-        errRet = {
-            "message": str(e),
-            "stack": trace
-        }
+        errRet = {"message": str(e), "stack": trace}
         print(errRet)
-        print("FC Invoke End RequestId: " + request_id +
-              ", Error: Unhandled function error")
+        print(
+            "FC Invoke End RequestId: "
+            + request_id
+            + ", Error: Unhandled function error"
+        )
         return errRet, 404, [("x-fc-status", "404")]
 
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=9000)
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=9000)
--- src/readme.md
+++ src/readme.md
@@ -1,52 +1,97 @@
-# start-word2pdf 帮助文档
 
+> 注：当前项目为 Serverless Devs 应用，由于应用中会存在需要初始化才可运行的变量（例如应用部署地区、函数名等等），所以**不推荐**直接 Clone 本仓库到本地进行部署或直接复制 s.yaml 使用，**强烈推荐**通过 `s init --project ${模版名称}` 的方法或应用中心进行初始化，详情可参考[部署 & 体验](#部署--体验) 。
+
+# start-word2pdf-v3 帮助文档
 <p align="center" class="flex justify-center">
     <a href="https://www.serverless-devs.com" class="ml-1">
-    <img src="http://editor.devsapp.cn/icon?package=start-word2pdf&type=packageType">
+    <img src="http://editor.devsapp.cn/icon?package=start-word2pdf-v3&type=packageType">
   </a>
-  <a href="http://www.devsapp.cn/details.html?name=start-word2pdf" class="ml-1">
-    <img src="http://editor.devsapp.cn/icon?package=start-word2pdf&type=packageVersion">
+  <a href="http://www.devsapp.cn/details.html?name=start-word2pdf-v3" class="ml-1">
+    <img src="http://editor.devsapp.cn/icon?package=start-word2pdf-v3&type=packageVersion">
   </a>
-  <a href="http://www.devsapp.cn/details.html?name=start-word2pdf" class="ml-1">
-    <img src="http://editor.devsapp.cn/icon?package=start-word2pdf&type=packageDownload">
+  <a href="http://www.devsapp.cn/details.html?name=start-word2pdf-v3" class="ml-1">
+    <img src="http://editor.devsapp.cn/icon?package=start-word2pdf-v3&type=packageDownload">
   </a>
 </p>
 
 <description>
 
-> ***快速部署一个word转pdf的应用到阿里云函数计算***
+快速部署一个word转pdf的应用到阿里云函数计算
 
 </description>
 
-<table>
-</table>
+<codeUrl>
 
-<codepre id="codepre">
-</codepre>
+- [:smiley_cat: 代码](https://github.com/devsapp/start-word2pdf/tree/V3/src)
 
-<deploy>
+</codeUrl>
+<preview>
+
+
+
+</preview>
+
+
+## 前期准备
+
+使用该项目，您需要有开通以下服务：
+
+<service>
+
+
+
+| 服务 |  备注  |
+| --- |  --- |
+| 函数计算 FC |  word转pdf的函数需要部署到函数计算 |
+| 对象存储 OSS |  原始word文件和转换后的pdf文件保存在对象存储OSS中 |
+
+</service>
+
+推荐您拥有以下的产品权限 / 策略：
+<auth>
+
+
+
+| 服务/业务 |  权限 |  备注  |
+| --- |  --- |   --- |
+| 函数计算 | AliyunFCFullAccess |  word转pdf的函数需要部署到函数计算 |
+
+</auth>
+
+<remark>
+
+
+
+</remark>
+
+<disclaimers>
+
+
+
+</disclaimers>
 
 ## 部署 & 体验
 
 <appcenter>
-
--  :fire:  通过 [Serverless 应用中心](https://fcnext.console.aliyun.com/applications/create?template=start-word2pdf) ，
-[![Deploy with Severless Devs](https://img.alicdn.com/imgextra/i1/O1CN01w5RFbX1v45s8TIXPz_!!6000000006118-55-tps-95-28.svg)](https://fcnext.console.aliyun.com/applications/create?template=start-word2pdf)  该应用。 
-
+   
+- :fire: 通过 [Serverless 应用中心](https://fcnext.console.aliyun.com/applications/create?template=start-word2pdf-v3) ，
+  [![Deploy with Severless Devs](https://img.alicdn.com/imgextra/i1/O1CN01w5RFbX1v45s8TIXPz_!!6000000006118-55-tps-95-28.svg)](https://fcnext.console.aliyun.com/applications/create?template=start-word2pdf-v3) 该应用。
+   
 </appcenter>
-
+<deploy>
+    
 - 通过 [Serverless Devs Cli](https://www.serverless-devs.com/serverless-devs/install) 进行部署：
-    - [安装 Serverless Devs Cli 开发者工具](https://www.serverless-devs.com/serverless-devs/install) ，并进行[授权信息配置](https://www.serverless-devs.com/fc/config) ；
-    - 初始化项目：`s init start-word2pdf -d start-word2pdf`   
-    - 进入项目，并进行项目部署：`cd start-word2pdf && s deploy -y`
-
+  - [安装 Serverless Devs Cli 开发者工具](https://www.serverless-devs.com/serverless-devs/install) ，并进行[授权信息配置](https://docs.serverless-devs.com/fc/config) ；
+  - 初始化项目：`s init --project start-word2pdf-v3 -d start-word2pdf-v3`
+  - 进入项目，并进行项目部署：`cd start-word2pdf-v3 && s deploy -y`
+   
 </deploy>
+
+## 应用详情
 
 <appdetail id="flushContent">
 
-# 应用详情
-
-项目部署完成，可以通过`invoke`命令进行触发/测试：
+项目部署完成，可以通过`invoke`命令或者在控制台使用事件进行触发/测试：
 
 ```bash
 # 调用
@@ -83,31 +128,40 @@ $ s invoke -e '{"word_file":"example.docx", "mark_text": "函数计算", "rotate
     
 ![](https://img.alicdn.com/imgextra/i4/O1CN01xJymEK1MP9YHRBkQx_!!6000000001426-2-tps-652-841.png)
 
-# 其他
-如果进行二次开发， 有新的依赖 python lib, 可以直接使用如下命令：
 
-```bash
-$ s build --use-sandbox
-# 进入 sandbox 后
-$ s-install pip install xxx
+## layer
 
-# exit 退出， 然后重新 s deploy 即可
+引入的 public layer 包含了 libreoffice 以及如下 python 第三方依赖
+
+```
+oss2==2.16.0
+flask==2.2.2
+Pillow==9.4.0
+PyPDF2==3.0.1
+reportlab==3.6.12
+cryptography==3.4.8
+urllib3==1.26.2
 ```
 
 </appdetail>
 
+## 使用文档
+
+<usedetail id="flushContent">
+</usedetail>
+
+
 <devgroup>
+
 
 ## 开发者社区
 
 您如果有关于错误的反馈或者未来的期待，您可以在 [Serverless Devs repo Issues](https://github.com/serverless-devs/serverless-devs/issues) 中进行反馈和交流。如果您想要加入我们的讨论组或者了解 FC 组件的最新动态，您可以通过以下渠道进行：
 
-<p align="center">
+<p align="center">  
 
 | <img src="https://serverless-article-picture.oss-cn-hangzhou.aliyuncs.com/1635407298906_20211028074819117230.png" width="130px" > | <img src="https://serverless-article-picture.oss-cn-hangzhou.aliyuncs.com/1635407044136_20211028074404326599.png" width="130px" > | <img src="https://serverless-article-picture.oss-cn-hangzhou.aliyuncs.com/1635407252200_20211028074732517533.png" width="130px" > |
-|--- | --- | --- |
-| <center>微信公众号：`serverless`</center> | <center>微信小助手：`xiaojiangwh`</center> | <center>钉钉交流群：`33947367`</center> | 
-
+| --------------------------------------------------------------------------------------------------------------------------------- | --------------------------------------------------------------------------------------------------------------------------------- | --------------------------------------------------------------------------------------------------------------------------------- |
+| <center>微信公众号：`serverless`</center>                                                                                         | <center>微信小助手：`xiaojiangwh`</center>                                                                                        | <center>钉钉交流群：`33947367`</center>                                                                                           |
 </p>
-
 </devgroup>
